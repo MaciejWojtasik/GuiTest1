72073dab9f2f969cc54513b62d4a36d5186feba4
MaciejWojtasik <maciej_wojtasik@o2.pl>
2015-04-19 14:35:43 +0200

    added scanf
    v1.1 bug fix for production

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@
 using namespace std;
 
 int main(){
-	cout << "Hello World!";
+	int a, b, c;
+	cout << "Hello World!" << endl;
+	cout << "Podaj 3 liczby:" << endl;
+	cin >> a >> b >> c;
 	
 	system("PAUSE");
 	return 0;
